a5efcc358084f946e0bba1735bce8c76177ec8cb
the-amazing-ultraton <guillermo.maschwitz@protofire.io>
2022-07-05 11:53:11 -0300

    feat: #GS-52 - configuration and branding

--- src/config/chain.d.ts
+++ src/config/chain.d.ts
@@ -13,6 +13,8 @@ export const CHAIN_ID: Record<ChainName, ChainId> = {
   ETHEREUM: '1',
   RINKEBY: '4',
   VOLTA: '73799',
+  CRONOS_MAINNET: '25',
+  CRONOS_TESTNET: '338',
 }
 
 // Values match that required of onboard and returned by CGW
