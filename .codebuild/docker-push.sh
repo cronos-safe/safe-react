059e8830bebbe6b13199d0d757dbba69ef864ba0
the-amazing-ultraton <106994129+the-amazing-ultraton@users.noreply.github.com>
2022-08-03 15:56:54 -0300

    fix: DevOps - changed docker tag

--- .codebuild/docker-push.sh
+++ .codebuild/docker-push.sh
@@ -8,6 +8,6 @@ now=$(date +%Y-%m-%d--%H-%M-%S)
 for image in react_nginx; do
   docker tag $image:latest $repository-$image:latest
   docker push $repository-$image:latest
-  docker tag $image:latest $repository-$image:$now
-  docker push $repository-$image:$now
+  docker tag $image:latest $repository-$image:${CODEBUILD_RESOLVED_SOURCE_VERSION:0:7}
+  docker push $repository-$image:${CODEBUILD_RESOLVED_SOURCE_VERSION:0:7}
 done
